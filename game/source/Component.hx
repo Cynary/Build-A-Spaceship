5fd98299d2c49b8fe90eb5781bdce2bf8934b032
Rodrigo Toste Gomes <rgomes@mit.edu>
2014-10-03 17:03:11 -0400

    Added files for ship/components model

---
+++ game/source/Component.hx
@@ -0,0 +1,17 @@
+package;
+
+// Add a 
+
+class Component
+{
+	var hpModifier:Int = 0;
+	var atkModifier:Int = 0;
+	var speedModifier:Int = 0;
+	var armor:Bool = false;
+	var capacityModifer:Int = 0;
+	
+	public static function main():Void
+	{
+		
+	}
+}

26210687fe40ca4d4b7138a4999b744d43692bb4
Rodrigo Toste Gomes <rgomes@mit.edu>
2014-10-07 21:53:22 -0400

    Added basic Component class as well - forgot it was needed for Ship class.

--- game/source/Component.hx
+++ game/source/Component.hx
@@ -1,17 +1,25 @@
 package;
 
-// Add a 
-
 class Component
 {
-	var hpModifier:Int = 0;
-	var atkModifier:Int = 0;
-	var speedModifier:Int = 0;
-	var armor:Bool = false;
-	var capacityModifer:Int = 0;
-	
-	public static function main():Void
+	private var defense:Int;
+	private var attack:Int;
+	private var speed:Int;
+	private var cargo:Int;
+	private var cost:Int;
+
+	public function new(defense:Int = 0, attack:Int = 0, speed:Int = 0, cargo:Int = 0, cost:Int = 0)
 	{
-		
+		this.defense = defense;
+		this.attack = attack;
+		this.speed = speed;
+		this.cargo = cargo;
+		this.cost = cost;
 	}
+
+	public function getDefense() { return defense; }
+	public function getAttack() { return attack; }
+	public function getSpeed() { return speed; }
+	public function getCargo() { return cargo; }
+	public function getcost() { return cost; }
 }

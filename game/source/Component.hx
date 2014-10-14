2ab168f8f359bbb4cdc283b7f1f2b36db957d3c8
Tej Chajed <tchajed@gmail.com>
2014-10-14 17:36:28 -0400

    Make component stat text dynamic
    - doesn't actually change during the game, but text is initialized
      based on the actual components
    - old static image text is still there, obviously would need to be
      removed

--- game/source/Component.hx
+++ game/source/Component.hx
@@ -8,8 +8,9 @@ class Component
 	private var cargo:Int;
 	private var cost:Int;
 	private var name:String;
+	private var shortDisplay:String;
 
-	public function new(defense:Int = 0, attack:Int = 0, speed:Int = 0, cargo:Int = 0, cost:Int = 0, name:String = "")
+	public function new(defense:Int = 0, attack:Int = 0, speed:Int = 0, cargo:Int = 0, cost:Int = 0, name:String = "", shortDisplay:String = "")
 	{
 		this.defense = defense;
 		this.attack = attack;
@@ -17,6 +18,7 @@ class Component
 		this.cargo = cargo;
 		this.cost = cost;
 		this.name = name;
+		this.shortDisplay = shortDisplay;
 	}
 
 	public function getDefense() { return defense; }
@@ -25,4 +27,28 @@ class Component
 	public function getCargo() { return cargo; }
 	public function getCost() { return cost; }
 	public function getName() { return name; }
+
+	private function statSummary(v:Int, code:String) : String {
+		if (v == 0) {
+			return "";
+		}
+		var s:String = "";
+		if (v > 0) {
+			s = "+";
+		}
+		s += v + code + " ";
+		return s;
+	}
+
+	public function summary():String {
+		var s:String = '$$${this.cost} ';
+		if (shortDisplay != "") {
+			s += '${this.shortDisplay} ';
+		}
+		s += statSummary(defense, "D");
+		s += statSummary(attack, "A");
+		s += statSummary(speed, "S");
+		s += statSummary(cargo, "C");
+		return s;
+	}
 }
